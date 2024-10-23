715e23be4e959828741235c0ab29107b12ce035b
damenor <damenor.develop@gmail.com>
2024-10-23 13:58:14 +0200

    Fixed

--- src/utils/supabase.utils.ts
+++ src/utils/supabase.utils.ts
@@ -4,8 +4,8 @@ export const SUPABASE_URL = import.meta.env.SUPABASE_URL
 export const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY
 export const SUPABASE_COOKIE_ACCESS_TOKEN = 'sb-access-token'
 export const SUPABASE_COOKIE_REFRESH_TOKEN = 'sb-refresh-token'
-export const SUPABASE_CALLBACK_URL = 'http://localhost:4321' + '/api/auth/callback'
-// export const SUPABASE_CALLBACK_URL = import.meta.env.WEBSITE_URL + '/api/auth/callback'
+// export const SUPABASE_CALLBACK_URL = 'http://localhost:4321' + '/api/auth/callback'
+export const SUPABASE_CALLBACK_URL = import.meta.env.WEBSITE_URL + '/api/auth/callback'
 
 export const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY, {
   auth: {
